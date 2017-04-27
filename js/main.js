15bbc3fb9a91e0fa5b85a40c2695a3499b130215
Cory <Coryjh85@gmail.com>
2017-04-27 14:23:01 -0700

    configured sass, worked on some js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 $(function(){
+  console.log('hi');
 
-// $('#selection').on('submit', function(event_object) {
-//   event_object.preventDefault();
-//   $('.articlegrid').empty();
-//   var choice = $('#selection').val();
-//   var articles = ''; 
-
-
-var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-url += '?' + $.param({
-  'api-key': 'a1f474fef60b4344b4a905fde1e852da'
 });
+
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+  'api-key': "a1f474fef60b4344b4a905fde1e852da"
+});
+var storyCategory = '';
+var sections = '';
+
 $.ajax({
   url: url,
   method: 'GET',
@@ -18,8 +17,12 @@ $.ajax({
   console.log(result);
 }).fail(function(err) {
   throw err;
-});
-});
-// });
+}).done(function(data){
+  $.each(data.results, function(index, value){
+    storyCategory += '<option class="dropdown-item" value="home">' + value.section + '</option>'
+    console.log(storyCategory)
+  }).slice(0, 12);
+   $("#selection").append(storyCategory);
 
+});
 
