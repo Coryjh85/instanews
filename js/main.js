38b3135fad3e101178877dc118e6d38029f8a442
Cory <Coryjh85@gmail.com>
2017-04-29 20:54:34 -0700

    first bits of css, minor js tweaks

--- js/main.js
+++ js/main.js
@@ -1,45 +1,47 @@
-$(function(){
-  
+$(function () {
+
   var $articleGrid = $('.articleGrid');
   $('select').change(function () {
     $articleGrid.empty();
-  
 
- $('select').change(function(){
-  
-var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/';
-  apiUrl+=$(this).val();
-  apiUrl+='.json';
-  apiUrl += '?' + $.param({'api-key': 'a1f474fef60b4344b4a905fde1e852da'});
-$.ajax({
-      url: apiUrl,
-      method: 'GET',
-    }).done(function(data) {
-    $.each(data.results, function(index, value){
-}); 
- var articleGroup = data.results.filter(function (item) {
-      return item.multimedia.length;
-    }).slice(0, 12);
 
-     $.each(articleGroup, function(key, value) {
-      var articleUrl = value.url;
-      var pic = value.multimedia[4].url;
-      var title = value.title;
-      var caption = value.abstract;
-      var results = '';
-    
+    $('select').change(function () {
+      var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/';
+      apiUrl += $(this).val();
+      apiUrl += '.json';
+      apiUrl += '?' + $.param({
+        'api-key': 'a1f474fef60b4344b4a905fde1e852da'
+      });
+      $.ajax({
+        url: apiUrl,
+        method: 'GET',
+      })
+        .done(function (data) {
+          $.each(data.results, function () {
+          });
+          var articleGroup = data.results.filter(function (item) {
+            return item.multimedia.length;
+          }).slice(0, 12);
 
-      results += '<li class="article" alt="'+ title +'" style="background-image: url(' + pic + ' )"> <a href=';
-      results += articleUrl;
-      results += '>';
-      results += caption;
-      results += ' </a> <p class="headline">';
-      results += '</p> </li>';
- 
+          $.each(articleGroup, function (key, value) {
+            var articleUrl = value.url;
+            var pic = value.multimedia[4].url;
+            var title = value.title;
+            var caption = value.abstract;
+            var results = '';
 
-    $('.articleGrid').append(results);
-  });  
-   });
+
+            results += '<li class="article" alt="' + title + '" style="background-image: url(' + pic + ' )"> <a href=';
+            results += articleUrl;
+            results += '>';
+            results += caption;
+            results += ' </a> <p class="headline">';
+            results += '</p> </li>';
+
+
+            $('.articleGrid').append(results);
+          });
+        });
+    });
   });
- });
 });
