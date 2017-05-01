fd19230ae23849d2148565b7179a8dd8a7ec7594
Cory <Coryjh85@gmail.com>
2017-05-01 11:46:48 -0700

    javascript pretty much done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(function () {
-
+$('.loadingGif').hide();
   var $articleGrid = $('.articleGrid');
   $('select').change(function () {
     $articleGrid.empty();
+     $('.loadingGif').show();
+  $('#logo').removeClass('logo').addClass('logoSmall');
 
+  });
 
     $('select').change(function () {
       var apiUrl = 'https://api.nytimes.com/svc/topstories/v2/';
@@ -15,15 +18,17 @@ $(function () {
       $.ajax({
         url: apiUrl,
         method: 'GET',
-      })
+      
+    })
+  
         .done(function (data) {
           $.each(data.results, function () {
           });
           var articleGroup = data.results.filter(function (item) {
             return item.multimedia.length;
           }).slice(0, 12);
-
-          $.each(articleGroup, function (key, value) {
+        
+        $.each(articleGroup, function (key, value) {
             var articleUrl = value.url;
             var pic = value.multimedia[4].url;
             var title = value.title;
@@ -38,10 +43,9 @@ $(function () {
             results += ' </a> <p class="headline">';
             results += '</p> </li>';
 
-
+            $('.loadingGif').hide();
             $('.articleGrid').append(results);
           });
         });
     });
-  });
-});
+ });
