1ded6636fb826805be3557c3d76c263f87387eb3
Cory <Coryjh85@gmail.com>
2017-05-01 21:18:52 -0700

    final tidy mk2

--- js/main.js
+++ js/main.js
@@ -14,34 +14,34 @@ $(function () {
     apiUrl += '?' + $.param({
       'api-key': 'a1f474fef60b4344b4a905fde1e852da'
     });
+
     $.ajax({
       url: apiUrl,
       method: 'GET',
-    })
-      .done(function (data) {
-        $.each(data.results, function () {
-        });
-        var articleGroup = data.results.filter(function (item) {
-          return item.multimedia.length;
-        }).slice(0, 12);
-
-        $.each(articleGroup, function (key, value) {
-          var articleUrl = value.url;
-          var pic = value.multimedia[4].url;
-          var title = value.title;
-          var caption = value.abstract;
-          var results = '';
-
-
-          results += '<li class="article" alt="' + title + '" style="background-image: url(' + pic + ' )"> <a href=';
-          results += articleUrl;
-          results += '><p class="headline">';
-          results += caption;
-          results += '</p> </a></li> ';
-
-          $('.loadingGif').hide();
-          $('.articleGrid').append(results);
-        });
+    }).done(function (data) {
+      $.each(data.results, function () {
       });
+
+      var articleGroup = data.results.filter(function (item) {
+        return item.multimedia.length;
+      }).slice(0, 12);
+
+      $.each(articleGroup, function (key, value) {
+        var articleUrl = value.url;
+        var pic = value.multimedia[4].url;
+        var title = value.title;
+        var caption = value.abstract;
+        var results = '';
+
+        results += '<li class="article" alt="' + title + '" style="background-image: url(' + pic + ' )"> <a href=';
+        results += articleUrl;
+        results += '><p class="headline">';
+        results += caption;
+        results += '</p> </a></li> ';
+
+        $('.loadingGif').hide();
+        $('.articleGrid').append(results);
+      });
+    });
   });
 });
